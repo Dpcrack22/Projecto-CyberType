ed3384fe0539e8d42860fdce1affe6bccbfb4309
Dpcrack22 <davidperera2006@gmail.com>
2025-10-27 16:49:21 +0100

    Update play.php

--- play.php
+++ play.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Play - CyberType</title>
     <link rel="stylesheet" href="./styles.css">
-    
 </head>
 <body id="bodyPlay">
     <h1>Preparat!</h1>
