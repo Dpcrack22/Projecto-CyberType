80b0e32d60188667c3056f4cbcfd9b708cecdaa9
Dpcrack22 <davidperera2006@gmail.com>
2025-10-27 02:35:38 +0100

    plantillas de archivos

---
+++ error404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404 - Page not found</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    
+</body>
+</html>
