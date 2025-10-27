24af83d9a5bc1e7359cb9026fc7b6f31b9c65875
Dpcrack22 <davidperera2006@gmail.com>
2025-10-27 16:43:56 +0100

    Update play.php

--- play.php
+++ play.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Play - CyberType</title>
     <link rel="stylesheet" href="./styles.css">
-    <script src="scriptPlay.js" defer></script>
+    
 </head>
 <body id="bodyPlay">
     <h1>Preparat!</h1>
@@ -15,5 +15,6 @@
         <p id="frase"></p>
         <input type="text" id="inputOcult" autofocus/>
     </div>
+    <script src="./scriptPlay.js" defer></script>
 </body>
 </html>
