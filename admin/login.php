2d6d7e6f2a4408028cc701f531b04fe341a839f9
Dpcrack22 <davidperera2006@gmail.com>
2025-11-04 16:03:52 +0100

    estilos cambiados

--- admin/login.php
+++ admin/login.php
@@ -25,7 +25,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login</title>
-    <link rel="stylesheet" type="text/css" href="/admin/styles.css?<?php echo time(); ?>" />
+    <link rel="stylesheet" type="text/css" href="/styles.css?<?php echo time(); ?>" />
 </head>
 <body class="body-loginAdmin">
     <h2>Login</h2>
