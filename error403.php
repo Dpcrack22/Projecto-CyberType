b3ad1666a7a09bccd716b849981418745ff0be17
Dpcrack22 <davidperera2006@gmail.com>
2025-11-04 18:16:39 +0100

    Update error403.php

--- error403.php
+++ error403.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Error 403 - Accés denegat</title>
-    <link rel="stylesheet" type="text/css" href="./styles.css?<?php echo time(); ?>" />
+    <link rel="stylesheet" type="text/css" href="/styles.css?<?php echo time(); ?>" />
 </head>
 <body class="body-403">
     <div>
