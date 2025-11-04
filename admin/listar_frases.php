17c2fbbc852f884a5b74adb156112172dc25006a
Dpcrack22 <davidperera2006@gmail.com>
2025-11-04 17:36:44 +0100

    ESTILOS CAMBIADOS

--- admin/listar_frases.php
+++ admin/listar_frases.php
@@ -16,7 +16,7 @@ $archivo = '../sentences.txt';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin listar frases</title>
-    <link rel="stylesheet" type="text/css" href="./styles.css?<?php echo time(); ?>" />
+    <link rel="stylesheet" type="text/css" href="/styles.css?<?php echo time(); ?>" />
 </head>
 <body class="body-listarFrases">
     <h1>Listado de frases</h1>
