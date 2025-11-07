3bc2684727606c64f92cec1f57a8d7c25f1e8a42
Dpcrack22 <davidperera2006@gmail.com>
2025-11-07 17:36:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
     session_start();
 
     require_once(__DIR__ . "/admin/log_function.php");
+    $arch_act="index.php";
 
     if (isset($_SESSION['playerName'])) {
-        registrarLog("El jugador '{$_SESSION['playerName']}' accedió al menú principal.");
+        registrarLog($arch_act,"El jugador '{$_SESSION['playerName']}' accedió al menú principal.");
     }
 
     if (isset($_SESSION['game_finished']) || isset($_SESSION['score']) || isset($_SESSION['bonus'])) {
