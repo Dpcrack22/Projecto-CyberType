2636d2ae4d1faa29f266fe3a38409bacebd916c4
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2025-11-10 15:39:02 +0100

    Cambios de estilos y posición de la imagen (provisional)

--- get_sentence.php
+++ get_sentence.php
@@ -1,26 +1,26 @@
 <?php
-session_name("jugadorSession");
-session_start();
+    session_name("jugadorSession");
+    session_start();
 
-$difficulty = $_POST['difficulty'] ?? '';
+    $difficulty = $_POST['difficulty'] ?? '';
 
-$archivo = './sentences.txt';
-$lineas = file($archivo, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $archivo = './sentences.txt';
+    $lineas = file($archivo, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-$frasesFiltradas = [];
-foreach ($lineas as $linea) {
-    list($nivelFrase, $frases) = explode('|', $linea, 2);
-    if ($nivelFrase === $difficulty) {
-        $frasesFiltradas = array_map('trim', explode(',', $frases));
-        break;
+    $frasesFiltradas = [];
+    foreach ($lineas as $linea) {
+        list($nivelFrase, $frases) = explode('|', $linea, 2);
+        if ($nivelFrase === $difficulty) {
+            $frasesFiltradas = array_map('trim', explode(',', $frases));
+            break;
+        }
     }
-}
 
-$fraseAleatoria = $frasesFiltradas[array_rand($frasesFiltradas)];
-list($soloFrase, $soloImagen) = explode('@@', $fraseAleatoria . '@@');
+    $fraseAleatoria = $frasesFiltradas[array_rand($frasesFiltradas)];
+    list($soloFrase, $soloImagen) = explode('@@', $fraseAleatoria . '@@');
 
-echo json_encode([
-    "frase" => $soloFrase,
-    "imagen" => $soloImagen
-]);
+    echo json_encode([
+        "frase" => $soloFrase,
+        "imagen" => $soloImagen
+    ]);
 ?>
